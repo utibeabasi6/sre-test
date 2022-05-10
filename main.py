5ef9f2c7aad63a86e9b012dc17f90b2cf35255b8
Utibeabasi Umanah <utibeabasiumanah6@gmail.com>
2022-05-10 17:31:25 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+def is_prime(num):
+    # Function to check if a number is prime or not
+
+    # prime numbers are greater than 1
+    if num > 1:
+        # check for factors
+        for i in range(2, num):
+            if (num % i) == 0:
+                # if factor is found, number is not prime
+                return False
+        else:
+            return True
+    return False
+
+def primes(num):
+    has_seen = set()
+    results = []
+    for i in range(num):
+        if is_prime(num):
+            if i in has_seen:
+                results.append(str(i) + "*")
+            else:
+                results.append(str(i))
+        else:
+            continue
+    return results
+
+print(primes(10))
